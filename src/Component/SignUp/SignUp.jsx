886f9bb4020499759cf3499058cefd64ae84c52d
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-01-12 23:32:25 +0600

    added password show toogle btn at sign up

--- src/Component/SignUp/SignUp.jsx
+++ src/Component/SignUp/SignUp.jsx
@@ -1,18 +1,27 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../../firebase/firebase.init";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const SignUp = () => {
   const [success, setSuccess] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const handleSignUp = (e) => {
     e.preventDefault();
     const form = e.target;
     const email = form.email.value;
     const password = form.password.value;
-    console.log(email, password);
+    const terms = form.terms.checked;
+    console.log(email, password, terms);
     setSuccess(false);
     setErrorMessage("");
+    if (!terms) {
+      setErrorMessage(
+        "You have to accecpt our Terms and Conditon before you get in."
+      );
+      return;
+    }
     // password validation
     const passwordRegExp = /(?=.*d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
     if (passwordRegExp.test(password) === false) {
@@ -46,16 +55,30 @@ const SignUp = () => {
               placeholder="Email"
             />
             <label className="label">Password</label>
-            <input
-              type="password"
-              name="password"
-              className="input"
-              placeholder="Password"
-            />
+            <div className="relative items-center">
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                className="input"
+                placeholder="Password"
+              />
+              <button
+                onClick={() => {
+                  setShowPassword(!showPassword);
+                }}
+                className="btn btn-ghost btn-xs absolute top-2 right-6"
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
             <div>
               <a className="link link-hover">Forgot password?</a>
             </div>
-            <button className="btn btn-ghost mt-4 w-1/4">Sign Up</button>
+            <label className="label">
+              <input type="checkbox" name="terms" className="checkbox" />
+              Accept terms and conditions
+            </label>
+            <button className="btn btn-naturel w-1/4">Sign Up</button>
           </form>
           {errorMessage && <p className="text-red-500">{errorMessage}</p>}
           {success && (
